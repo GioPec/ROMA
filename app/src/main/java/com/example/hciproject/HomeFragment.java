8ede63bd58cc73c615583c7062f9c4316503af9e
Giovanni Pecorelli <giopec98@gmail.com>
2021-05-05 14:02:55 +0200

    Integrazione Google Maps

--- app/src/main/java/com/example/hciproject/HomeFragment.java
+++ app/src/main/java/com/example/hciproject/HomeFragment.java
@@ -4,6 +4,8 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,9 +13,11 @@ import android.view.ViewGroup;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMapOptions;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class HomeFragment extends Fragment {
@@ -22,8 +26,6 @@ public class HomeFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_home, container, false);
     }
-
-    /*
 
     private OnMapReadyCallback callback = new OnMapReadyCallback() {
 
@@ -35,22 +37,36 @@ public class HomeFragment extends Fragment {
          * If Google Play services is not installed on the device, the user will be prompted to
          * install it inside the SupportMapFragment. This method will only be triggered once the
          * user has installed Google Play services and returned to the app.
+         */
 
         @Override
         public void onMapReady(GoogleMap googleMap) {
-            LatLng sydney = new LatLng(-34, 151);
-            googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-            googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+            googleMap.getUiSettings().setMyLocationButtonEnabled(true);
+            googleMap.setMinZoomPreference(10);
+            googleMap.setMaxZoomPreference(20);
+
+            // Map bounds
+            LatLngBounds romeBounds = new LatLngBounds(
+                    new LatLng(41.77, 12.364), // SW bounds
+                    new LatLng(42.0, 12.65)  // NE bounds
+            );
+            googleMap.setLatLngBoundsForCameraTarget(romeBounds);
+
+            /* Custom marker
+            int height = 100;
+            int width = 100;
+            BitmapDrawable bitmapdraw = (BitmapDrawable)getResources().getDrawable(R.mipmap.marker);
+            Bitmap b = bitmapdraw.getBitmap();
+            Bitmap smallMarker = Bitmap.createScaledBitmap(b, width, height, false);
+            */
+
+            LatLng rome = new LatLng(41.9, 12.5);
+            googleMap.addMarker(new MarkerOptions().position(rome).title("Buca"));
+            //googleMap.moveCamera(CameraUpdateFactory.newLatLng(rome));
+
         }
     };
-
-    @Nullable
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             @Nullable ViewGroup container,
-                             @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
-    }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
@@ -61,6 +77,4 @@ public class HomeFragment extends Fragment {
             mapFragment.getMapAsync(callback);
         }
     }
-
-    */
 }
